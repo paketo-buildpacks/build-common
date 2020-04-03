5ec65acd5536c8a9e64f6a13ac53221a2b495d21
Ben Hale <bhale@pivotal.io>
2020-04-03 14:10:36 -0700

    Update Builder Dependency Script
    Signed-off-by: Ben Hale <bhale@pivotal.io>

---
+++ update-builder-dependency.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# shellcheck source=common.sh
+source "$(dirname "$0")"/common.sh
+
+printf "➜ Building Dependency Updater\n"
+GO111MODULE=on GOPRIVATE="*" go get -ldflags='-s -w' github.com/paketo-buildpacks/libpak/cmd/update-builder-dependency
+
+printf "➜ Updating Dependency\n"
+update-builder-dependency \
+  --builder-toml "${ROOT}"/source/builder.toml \
+  --id "${DEPENDENCY}" \
+  --version "$(version)"
+
+cd "${ROOT}"/source
+
+git add buildpack.toml
+git checkout -- .
+
+git \
+  -c user.name='Paketo Robot' \
+  -c user.email='robot@paketo.io' \
+  commit \
+  --signoff \
+  --message "Dependency Upgrade: ${DEPENDENCY} $(version)"
