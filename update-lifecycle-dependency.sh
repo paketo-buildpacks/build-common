87d2bcd05244465968929731dbb63c78e684c4f0
Ben Hale <bhale@pivotal.io>
2020-04-16 11:57:53 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- update-lifecycle-dependency.sh
+++ update-lifecycle-dependency.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # shellcheck source=common.sh
 source "$(dirname "$0")"/common.sh
 
-VERSION=$(cat "${ROOT}"/dependency/version)
+VERSION=$(cat "${ROOT}"/lifecycle/version)
 
 printf "➜ Building Dependency Updater\n"
 GO111MODULE=on GOPRIVATE="*" go get -ldflags='-s -w' github.com/paketo-buildpacks/libpak/cmd/update-lifecycle-dependency
@@ -25,4 +25,4 @@ git \
   -c user.email='robot@paketo.io' \
   commit \
   --signoff \
-  --message "Dependency Upgrade: ${DEPENDENCY} ${VERSION}"
+  --message "Dependency Upgrade: Lifecycle ${VERSION}"
