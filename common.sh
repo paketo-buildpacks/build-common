2c0848c3934edd86343a15d3abc4c322331bf2ea
Ben Hale <bhale@vmware.com>
2020-09-14 07:28:10 -0700

    Revert "Only configure credentials is variable has value"
    This reverts commit 8a1e8852857607f349b54e6bafff9e026ecd2a16.

--- common.sh
+++ common.sh
@@ -20,7 +20,7 @@ if command -v docker-registry &> /dev/null; then
   docker-registry serve /etc/docker/registry/config.yml &> /dev/null &
 fi
 
-if [[ -z "${DOCKER_REGISTRY_CREDENTIALS}" ]]; then
+if [[ -n "${DOCKER_REGISTRY_CREDENTIALS+x}" ]]; then
   printf "➜ Configuring Docker Registry Credentials\n"
   mv "${HOME}/.docker/config.json" "${HOME}/.docker/config.json.orig"
 
@@ -31,7 +31,7 @@ if [[ -z "${DOCKER_REGISTRY_CREDENTIALS}" ]]; then
      > "${HOME}/.docker/config.json"
 fi
 
-if [[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
+if [[ -n "${GOOGLE_APPLICATION_CREDENTIALS+x}" ]]; then
   printf "➜ Configuring Google Application Credentials\n"
   echo "${GOOGLE_APPLICATION_CREDENTIALS}" > "${ROOT}"/google-application-credentials.json
   export GOOGLE_APPLICATION_CREDENTIALS="${ROOT}"/google-application-credentials.json
